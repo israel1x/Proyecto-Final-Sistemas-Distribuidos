1ecc0f34ebe83c78da4807f4d75a7b0f9849b7f6
josealcivar <jose_alcivargarcia@hotmail.com>
2018-01-26 08:36:50 -0500

    rendimientos y pruebas

--- performance/rendimiento.py
+++ performance/rendimiento.py
@@ -5,13 +5,13 @@ data = {}
 data[' mysql'] = [6.1, 6.3, 6.2, 6.1, 5.7, 6.1, 6.3, 6.7, 5.3, 5.5]
 data[' redis + db'] = [70.9, 75.6, 84, 90, 58.7, 91.8, 68.4, 96.4, 74.6, 88]
 
-color_dict = {'DB mysql':'white', 'Redis + DB':'white'}
-controls = ['DB mysql', 'Redis +DB']
+color_dict = {' mysql':'white', ' redis + db':'white'}
+controls = [' mysql', ' redis + db']
 
 fig, ax = plt.subplots()
 
 boxplot_dict = ax.boxplot(
-    [data[x] for x in ['DB mysql', 'Redis +DB']],
+    [data[x] for x in [' mysql', ' redis + db']],
     positions = [1, 1.5],
     labels = controls,
     patch_artist = True,
@@ -23,6 +23,6 @@ for b in boxplot_dict['boxes']:
     b.set_facecolor(color_dict[lab])
     i += 1
 
-ax.set_ylim([0,np.max(data['Redis'])+10])
+ax.set_ylim([0,np.max(data[' redis + db'])+10])
 plt.ylabel("Throughput (Requests per second)")
 plt.show()
--- performance/rendimientodb.py
+++ performance/rendimientodb.py
@@ -2,16 +2,16 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 data = {}
-data['Just DB'] = [24524, 24333, 24409, 23355, 24459, 24417, 25937, 25511, 25130, 24143]
-data['Redis +DB'] = [3432, 3354, 2027, 4888, 6872, 764, 3790, 1413, 1825, 2383]
+data['mysql'] = [24524, 24333, 24409, 23355, 24459, 24417, 25937, 25511, 25130, 24143]
+data['redis + db'] = [3432, 3354, 2027, 4888, 6872, 764, 3790, 1413, 1825, 2383]
 
-color_dict = {'Just DB':'white', 'Redis +DB':'white'}
-controls = ['Just DB', 'Redis +DB']
+color_dict = {'mysql':'white', 'redis + db':'white'}
+controls = ['mysql', 'redis + db']
 
 fig, ax = plt.subplots()
 
 boxplot_dict = ax.boxplot(
-    [data[x] for x in ['Just DB', 'Redis +DB']],
+    [data[x] for x in ['mysql', 'redis + db']],
     positions = [1, 1.5],
     labels = controls,
     patch_artist = True,
@@ -23,6 +23,6 @@ for b in boxplot_dict['boxes']:
     b.set_facecolor(color_dict[lab])
     i += 1
 
-ax.set_ylim([0,np.max(data['mysql db'])+500])
+ax.set_ylim([0,np.max(data['mysql'])+500])
 plt.ylabel("Average latency (Miliseconds)")
 plt.show()
